f8dd2f93e4f706ba0c21e6cded4f53c59b614a6b
Dima Dubovski <dzmitry.duboyski@gmail.com>
2020-04-17 15:35:20 +0300

    refactor: add modules, del logs

---
+++ js/controlKey.js
@@ -0,0 +1,14 @@
+export default [
+    "CapsLock",
+    "Shift",
+    "Control",
+    "Meta",
+    "Alt",
+    " ",
+    "Backspace",
+    "Enter",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+  ];
